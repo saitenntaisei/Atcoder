b974644b2422f0802a8e2af17775e10de29edaa4
saiten <saitenntaisei@gmail.com>
2021-06-05 23:44:43 +0900

    commented out the code
    コードをコメントアウトした

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
+typedef long long ll;
+#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define show(s) cout << s << endl
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+#define LINF (1LL << 50)
+#define MOD (1e9 + 7)
+#define rrep(i, n) for (int i = n - 1; i >= 0; --i)
+typedef pair<ll, ll> P;
 
 int main()
 {
